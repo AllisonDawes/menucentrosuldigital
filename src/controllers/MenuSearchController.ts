229dbf17c4a67b0f795c0f200841fa475c353c96
AllisonDawes <allyssondawes@hotmail.com>
2021-07-16 23:56:41 -0300

    created: searchmenus

--- src/controllers/MenuSearchController.ts
+++ src/controllers/MenuSearchController.ts
@@ -5,13 +5,11 @@ import FindSearchMenuByNameService from "../services/FindSearchMenuByNameService
 
 class MenuSearchController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const user_id = request.user.id;
     const { name_store, uf, city } = request.query;
 
     const findSearchMenuByName = new FindSearchMenuByNameService();
 
     const menus = await findSearchMenuByName.execute({
-      user_id,
       name_store: String(name_store),
       uf: String(uf),
       city: String(city),
--- src/services/FindSearchMenuByNameService.ts
+++ src/services/FindSearchMenuByNameService.ts
@@ -3,24 +3,18 @@ import { getRepository, ILike } from "typeorm";
 import Menu from "../models/Menu";
 
 interface IRequest {
-  user_id: string;
   name_store: string;
   uf: string;
   city: string;
 }
 
 class FindMenuByNameService {
-  public async execute({
-    user_id,
-    name_store,
-    uf,
-    city,
-  }: IRequest): Promise<Menu[]> {
+  public async execute({ name_store, uf, city }: IRequest): Promise<Menu[]> {
     const menuRepository = getRepository(Menu);
 
     const menusSearch = await menuRepository.find({
       where: { name_store: ILike(`%${name_store}%`) },
-      relations: ["user", "address", "favorites"],
+      relations: ["user", "address"],
       order: { name_store: "ASC" },
     });
 
